91e42fdaff6328094b6cb7ac72044216c2ecd0b1
Justin Meng <jmeng2@uchicago.edu>
2025-10-27 18:42:49 -0500

    adjusted admin perms

--- cloudysky/app/admin.py
+++ cloudysky/app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Profile, Post, Comment, ModerationReason, PostMedia
+
+# Register your models here so they appear on the admin site.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(ModerationReason)
+admin.site.register(PostMedia)
